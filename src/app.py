410c6e1025b749370bc0bab7359ee8cd752ae373
Thiago Lopes <lopestbl@gmail.com>
2020-05-27 15:40:47 -0300

    Add CandidatesRepository

--- src/app.py
+++ src/app.py
@@ -1,36 +1,20 @@
-import pika
-import time
-import sys
-import threading
 from flask import Flask
-from consumer import Consumer
+from flask import request
+from flask import jsonify
 
+from candidates_repository import CandidatesRepository
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, cruel world!!!'
+def similar_candidates():
+    candidate_id = int(request.args.get('q'))
 
-def start_server():
-    print('Starting Flask server')
-    app.run(debug=True, host='0.0.0.0')
+    similar_candidates = CandidatesRepository().get_similar(candidate_id)
 
-def start_consumer():
-    connect_consumer()
-
-def connect_consumer(retries=0):
-    print('Connecting to RabbitMQ server')
-    try:
-        Consumer().start()
-    except pika.exceptions.AMQPConnectionError:
-        if retries >= 2:
-            raise
-
-        print('Connection error. Retrying in 5 seconds.')
-        time.sleep(5)
-        connect_consumer(retries + 1)
+    return jsonify(similar_candidates)
 
 if __name__ == '__main__':
-    threading.Thread(target=start_consumer).start()
-    start_server()
+    CandidatesRepository().start()
+
+    app.run(debug=True, host='0.0.0.0')
---
+++ src/candidates_repository.py
@@ -0,0 +1,65 @@
+from threading import Lock
+from typing import Optional
+from os import path
+import turicreate
+
+class SingletonMeta(type):
+    _instance = None
+    _lock: Lock = Lock()
+
+    def __call__(cls, *args, **kwargs):
+        with cls._lock:
+            if not cls._instance:
+                cls._instance = super().__call__(*args, **kwargs)
+
+        return cls._instance
+
+class CandidatesRepository(metaclass=SingletonMeta):
+    _model = None
+
+    # TODO read weights from env vars or something else
+    _weights = {
+        'career': 0.5,
+        'main_skills': 0.7,
+        'secondary_skills': 0.3,
+        'companies': 0.7,
+        'jobs': 0.7,
+        'education': 0.7,
+        'contract_type': 0.3,
+        'wage_claim': 0.3
+    }
+
+    # TODO new thread, train, and sync?
+    def update(self) -> None:
+        self.start()
+
+    def start(self) -> None:
+        self._model = self._train_model()
+
+    def get_similar(self, id: int) -> dict:
+        candidates = self._model.recommend_from_interactions([id], 10)
+        result = []
+
+        for candidate in candidates[0:10]:
+            result.append({
+                'id': candidate['candidate_id'],
+                'score': candidate['score']
+            })
+
+        return result
+
+    def _train_model(self):
+        # TODO read data from S3
+        data = turicreate.SFrame(self._data_path())
+
+        return turicreate.recommender.item_content_recommender.create(
+            data, 'candidate_id', weights=self._weights
+        )
+
+    def _data_path(self):
+        basepath = path.dirname(__file__)
+        filepath = path.abspath(
+            path.join(basepath, 'data', 'marketplace-candidates.csv')
+        )
+
+        return filepath
